cc91720233029b3368565da3d58d3827daeedc85
EmirYilmazV <emiryilmaz2505@gmail.com>
2024-10-04 20:57:51 +0300

    GeometrikOrtalamaHesaplama/Kayityeri

---
+++ GeometrikOrtalamaHesaplayıcı.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+
+{
+    static void Main()
+    {
+        Console.WriteLine("*----------------------------*");
+        Console.WriteLine("Geometrik Ortalama Hesaplayici");
+        Console.WriteLine("*----------------------------*");
+ 
+
+
+
+        Console.WriteLine("Lütfen ilk sayiyi giriniz");
+        int sayi1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Lütfen ikinci sayiyi giriniz");
+        int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+        double gOrtalama = Math.Sqrt(sayi1 * sayi2);
+        Console.WriteLine("Sayilarin Geometrik ortalamasi:{0}", gOrtalama);
+        Console.ReadLine();
+
+    }
+ 
+
+
+
+
+}
---
+++ Kayityeri.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Adinizi Ve Soyadinizi Giriniz");
+        Console.WriteLine("↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓");
+        string adsoyad = Console.ReadLine();
+
+        Console.WriteLine("Öğrenci Numaranizi Giriniz");
+        Console.WriteLine("↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓");
+        string ogrencino = Console.ReadLine();
+
+        Console.WriteLine("Telefon Numaranizi Giriniz");
+        Console.WriteLine("↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓");
+        string numara = Console.ReadLine();
+
+
+        Console.WriteLine("*----------------------------*");
+        Console.WriteLine("Adi Soyadi: " + adsoyad);
+        Console.WriteLine("Öğrenci Numarasi: " + ogrencino);
+        Console.WriteLine("Numarasi: " + numara);
+        Console.WriteLine("*----------------------------*");
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
