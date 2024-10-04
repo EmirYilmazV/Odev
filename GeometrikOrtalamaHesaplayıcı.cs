3b87877c1ab2eccc5fac20f2f9ef65935738f6e5
EmirYilmazV <emiryilmaz2505@gmail.com>
2024-10-04 21:36:39 +0300

    Update GeometrikOrtalamaHesaplayıcı.cs

--- GeometrikOrtalamaHesaplayıcı.cs
+++ GeometrikOrtalamaHesaplayıcı.cs
@@ -5,20 +5,20 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("*----------------------------*");
-        Console.WriteLine("Geometrik Ortalama Hesaplayici");
+        Console.WriteLine("*----------------------------*"); 
+        Console.WriteLine("Geometrik Ortalama Hesaplayici");  // Extra detay olsun diye bunları ekledim
         Console.WriteLine("*----------------------------*");
  
 
 
 
-        Console.WriteLine("Lütfen ilk sayiyi giriniz");
+        Console.WriteLine("Lütfen ilk sayiyi giriniz");   
         int sayi1 = Convert.ToInt32(Console.ReadLine());
 
         Console.WriteLine("Lütfen ikinci sayiyi giriniz");
         int sayi2 = Convert.ToInt32(Console.ReadLine());
 
-        double gOrtalama = Math.Sqrt(sayi1 * sayi2);
+        double gOrtalama = Math.Sqrt(sayi1 * sayi2);          // Gördüğüm bir işlem komudunu geometrik ortalama için kullandım         
         Console.WriteLine("Sayilarin Geometrik ortalamasi:{0}", gOrtalama);
         Console.ReadLine();
 
