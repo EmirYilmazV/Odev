36a48d7cbb5f6b81286ad9f82312859b43287e48
EmirYilmazV <emiryilmaz2505@gmail.com>
2024-10-04 21:37:58 +0300

    Update Kayityeri.cs

--- Kayityeri.cs
+++ Kayityeri.cs
@@ -5,7 +5,7 @@ class Program
     static void Main()
     {
         Console.WriteLine("Adinizi Ve Soyadinizi Giriniz");
-        Console.WriteLine("↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓");
+        Console.WriteLine("↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓"); // İlk önce gereken şeyleri yazıcağımız yerleri ayarladım
         string adsoyad = Console.ReadLine();
 
         Console.WriteLine("Öğrenci Numaranizi Giriniz");
@@ -17,8 +17,8 @@ class Program
         string numara = Console.ReadLine();
 
 
-        Console.WriteLine("*----------------------------*");
-        Console.WriteLine("Adi Soyadi: " + adsoyad);
+        Console.WriteLine("*----------------------------*");    // Hepsi bittikten sonra ise panel tarzında yazılan her bilginin gözüktüğü yeri yaptım
+        Console.WriteLine("Adi Soyadi: " + adsoyad);               
         Console.WriteLine("Öğrenci Numarasi: " + ogrencino);
         Console.WriteLine("Numarasi: " + numara);
         Console.WriteLine("*----------------------------*");
